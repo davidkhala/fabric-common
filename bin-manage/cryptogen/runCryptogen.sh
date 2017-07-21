53268c8e1144a2c1e58ec8b2fef9f8499c681881
Yuxiang LIU <david-khala@hotmail.com>
2017-07-21 15:56:09 +0800

    update tokenize

--- bin-manage/cryptogen/runCryptogen.sh
+++ bin-manage/cryptogen/runCryptogen.sh
@@ -2,22 +2,45 @@
 CURRENT="$(dirname $(readlink -f ${BASH_SOURCE}))"
 CONFIG_OUTPUT=$CURRENT/crypto-config
 CONFIG_INPUT=$CURRENT/cryptogen.yaml
-# clear existing
-MODE=$1
+
+BIN_PATH="$CURRENT/../../bin"
+
 
 function clearOutput() {
-	echo "clear CONFIG_OUTPUT $CONFIG_OUTPUT"
+    echo "clear CONFIG_OUTPUT $CONFIG_OUTPUT"
 	rm -rf $CONFIG_OUTPUT
 }
+remain_params=""
+for (( i = 1; i <= $#; i ++ )); do
+    j=${!i}
+    remain_params="$remain_params $j"
+done
 
-function gen() {
-	cd $CURRENT/../../bin
 
-	./cryptogen generate --config="$CONFIG_INPUT" --output="$CONFIG_OUTPUT"
+while getopts "ci:o:" shortname $remain_params; do
+    case $shortname in
+        c)
+            clearOutput
+        ;;
+        i)
+            echo "set cryptogen.yaml --config $OPTARG"
+            CONFIG_INPUT="$OPTARG"
+        ;;
+        o)
+            echo "set crypto-config/ folder --output $OPTARG"
+            CONFIG_OUTPUT="$OPTARG"
+        ;;
+        ?) #当有不认识的选项的时候arg为?
+            echo "unknown argument"
+            exit 1
+        ;;
+    esac
+done
 
-	cd $CURRENT
-}
-if [ "${MODE}" == "clear" ]; then
-	clearOutput
-fi
-gen
+
+# gen
+cd $BIN_PATH
+
+./cryptogen generate --config="$CONFIG_INPUT" --output="$CONFIG_OUTPUT"
+
+cd -
