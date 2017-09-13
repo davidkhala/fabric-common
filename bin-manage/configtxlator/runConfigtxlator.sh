4fee2e903a387e37262aa0d8e31f4f8793b2e56a
Yuxiang LIU <david-khala@hotmail.com>
2017-09-13 15:50:43 +0800

    fix:[tcp6       0      0 127.0.0.1:7059          127.0.0.1:60350         TIME_WAIT   - ]

--- bin-manage/configtxlator/runConfigtxlator.sh
+++ bin-manage/configtxlator/runConfigtxlator.sh
@@ -5,13 +5,13 @@ BIN_PATH="$CURRENT/../../bin"
 cd $BIN_PATH
 ACTION=$1
 if [ "$ACTION" == "start" ]; then
-
-	if netstat -pant | grep '7059'; then
+	# NOTE: some unknown process occupied 7059 when no configtxlator => [tcp6       0      0 127.0.0.1:7059          127.0.0.1:60350         TIME_WAIT   -]
+	if netstat -pant | grep '7059' | grep 'LISTEN'; then
 		echo 7059 occupied, skip
 	else
 		./configtxlator start &>/dev/null &
 	fi
-elif [ "$ACTION" == "down" ];then
-	pid=$(netstat -pant | grep '7059' | awk '{split($7, a, "/");print a[1]}')
+elif [ "$ACTION" == "down" ]; then
+	pid=$(netstat -pant | grep '7059' | grep 'LISTEN' | awk '{split($7, a, "/");print a[1]}')
 	kill $pid
 fi
