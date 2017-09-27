14e856781e7a972c698d32b4183666ca3c4212bf
Yuxiang LIU <david-khala@hotmail.com>
2017-09-27 12:08:04 +0800

    upgrade to 1.0.1

--- bin/get-byfn.sh
+++ bin/get-byfn.sh
@@ -8,6 +8,6 @@
 # This script downloads the build your first network sample app 
 
 # set the default Docker namespace and tag
-VERSION=1.0.0
+VERSION=1.0.1
 echo "===> Downloading Build Your First Network sample application"
 curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/examples/hyperledger-fabric-byfn-$(VERSION).tar.gz | tar xz
--- bin/get-docker-images.sh
+++ bin/get-docker-images.sh
@@ -8,7 +8,7 @@
 # set the default Docker namespace and tag
 DOCKER_NS=hyperledger
 ARCH=x86_64
-VERSION=1.0.0
+VERSION=1.0.1
 BASE_DOCKER_TAG=x86_64-0.3.1
 
 # set of Hyperledger Fabric images
