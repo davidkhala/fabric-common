08804f04a082c956bd845b48fb3f9dab11c02ce2
Yuxiang LIU <david-khala@hotmail.com>
2017-10-03 16:28:04 +0800

    rename from INSTALL to install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+CURRENT="$(dirname $(readlink -f ${BASH_SOURCE}))"
+
+$CURRENT/docker/nodejs/install.sh
+$CURRENT/docker/install.sh
+
+function golang() {
+	# install golang
+	sudo add-apt-repository ppa:longsleep/golang-backports -y
+	sudo apt-get update
+	sudo apt-get install -y golang-go
+}
+
+sudo apt-get upgrade -y
