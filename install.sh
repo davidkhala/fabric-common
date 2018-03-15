f71dd109b1f188396dbd0137d4fd90a92ab29aa0
davidliu <david-khala@hotmail.com>
2018-03-15 12:11:33 +0800

    TODO refine golang install

--- install.sh
+++ install.sh
@@ -12,41 +12,49 @@ for ((i = 2; i <= ${#}; i++)); do
 done
 
 function golang() {
-    goTar=go1.9.2.linux-amd64.tar.gz
-    if go version >/dev/null;then
-        echo ... to overwrite exiting golang at GOROOT: $(go env GOROOT)
-        sudo rm -rf $(go env GOROOT)
-    fi
-	# install golang
-	if docker version >/dev/null ;then
-        goTar=$(docker version | grep -m1 go| awk '{print($3)}').linux-amd64.tar.gz
-        echo ... to use docker inline go version :[${goTar}]
-    fi
-	wget https://redirector.gvt1.com/edgedl/go/${goTar}
-	sudo tar -C /usr/local -xzf ${goTar}
+	goVersion=go1.9.2
+	if docker version >/dev/null; then
+		goVesion=$(docker version | grep -m1 go | awk '{print($3)}')
+		echo ... to use docker inline go version :[${goVersion}]
+	fi
+
+	goTar=$goVersion.linux-amd64.tar.gz
 	# write path to 'go' command
 	if ! grep "/usr/local/go/bin" $systemProfile; then
 		echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a $systemProfile
+		export PATH=$PATH:/usr/local/go/bin
 	fi
+
+	if ! go version | grep $goVersion; then
+		if go version; then
+			echo ... to overwrite exiting golang at GOROOT: $(go env GOROOT)
+			sudo rm -rf $(go env GOROOT)
+		fi
+		wget https://redirector.gvt1.com/edgedl/go/${goTar}
+		sudo tar -C /usr/local -xzf ${goTar}
+		rm -f ${goTar}
+	fi
+
 	# write path to $GOPATH/bin
 	GOPATH=$(go env GOPATH)
 	if ! grep "$GOPATH/bin" $systemProfile; then
 		echo "export PATH=\$PATH:$GOPATH/bin" | sudo tee -a $systemProfile
+		export PATH=$PATH:$GOPATH/bin
 	fi
-	# delete install pack
-	rm -f ${goTar}
+
 }
+
 function golang-uninstall() {
 	:
 	#    TODO  To remove an existing Go installation from your system delete the go directory. This is usually /usr/local/go under Linux, Mac OS X, and FreeBSD or c:\Go under Windows.
 	# You should also remove the Go bin directory from your PATH environment variable. Under Linux and FreeBSD you should edit /etc/profile or $HOME/.profile. If you installed Go with the Mac OS X package then you should remove the /etc/paths.d/go file. Windows users should read the section about setting environment variables under Windows.
 }
-function govendor(){
-    go get -u github.com/kardianos/govendor
+function govendor() {
+	go get -u github.com/kardianos/govendor
 }
-function cn(){
-  	$CURRENT/docker/install.sh cn
-  	$CURRENT/docker/nodejs/install.sh cn
+function cn() {
+	$CURRENT/docker/install.sh cn
+	$CURRENT/docker/nodejs/install.sh cn
 }
 if [ -n "$fcn" ]; then
 	$fcn $remain_params
