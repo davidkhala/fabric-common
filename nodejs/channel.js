f2130eafb199ed94fb6a5099b19192938a5b9095
davidliu <david-khala@hotmail.com>
2018-05-08 17:08:03 +0800

    channel name follow reg, not to use toLowerCase

---
+++ nodejs/channel.js
@@ -0,0 +1,16 @@
+/**
+ * New feature introduced from 1.1.0-alpha
+ */
+const ChannelEventHub = require('fabric-client/lib/ChannelEventHub');
+const util = require('util');
+exports.newEventHub = (channel, peer) => {
+	channel.newChannelEventHub(peer);
+};
+exports.nameMatcher = (channelName, toThrow) => {
+	const namePattern = /^[a-z][a-z0-9.-]*$/;//TODO in fabric 1.2: we could test with new Channel()
+	const result = channelName.match(namePattern);
+	if (!result && toThrow) {
+		throw new Error(util.format('Failed to create Channel. channel name should match Regex %s, but got %j', namePattern, channelName));
+	}
+	return result;
+};
