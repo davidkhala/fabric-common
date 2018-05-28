3c9b061d69533d29ad28801a71aa34aabfca82e5
davidliu <david-khala@hotmail.com>
2018-05-28 10:32:06 +0800

    FIX key as a ECDSA_KEY instead of a string

--- nodejs/user.js
+++ nodejs/user.js
@@ -4,6 +4,7 @@ const path = require('path');
 const pathUtil = require('./path');
 const clientUtil = require('./client');
 const logger = require('./logger').new('userUtil');
+const ECDSA_KEY = require('fabric-client/lib/impl/ecdsa/key')
 exports.formatUsername = (username, domain) => `${username}@${domain}`;
 const User = require('fabric-client/lib/User');
 /**
@@ -14,7 +15,7 @@ const User = require('fabric-client/lib/User');
  * @param cryptoSuite
  * @returns {Promise|*|Promise<User>}
  */
-exports.loadFromLocal = async (cryptoPath, nodeType, {mspId}, cryptoSuite=clientUtil.newCryptoSuite()) => {
+exports.loadFromLocal = async (cryptoPath, nodeType, {mspId}, cryptoSuite = clientUtil.newCryptoSuite()) => {
 
 
 	const username = cryptoPath.userName;
@@ -22,14 +23,19 @@ exports.loadFromLocal = async (cryptoPath, nodeType, {mspId}, cryptoSuite=client
 	if (!exist) return;
 	const {keystore, signcerts} = exist;
 
-	const user = await exports.build(username, {key:fs.readFileSync(keystore), certificate: fs.readFileSync(signcerts)}, mspId, cryptoSuite);
+	const user = await exports.build(username, {key: fs.readFileSync(keystore), certificate: fs.readFileSync(signcerts)}, mspId, cryptoSuite);
 
 	return user;
 };
-exports.build = async (username, {key, certificate}, MSPID, cryptoSuite) => {
+exports.build = async (username, {key, certificate}, MSPID, cryptoSuite = clientUtil.newCryptoSuite()) => {
 	const user = new User(username);
-	//FIXME: importKey.then is not function in some case;
-	const privateKey = await cryptoSuite.importKey(key, {ephemeral: true});
+	let privateKey;
+	if (key instanceof ECDSA_KEY) {
+		privateKey = key;
+	} else {
+		//FIXME: importKey.then is not function in some case;
+		privateKey = await cryptoSuite.importKey(key, {ephemeral: true});
+	}
 	user.setCryptoSuite(cryptoSuite);
 	await user.setEnrollment(privateKey, certificate, MSPID);
 	return user;
