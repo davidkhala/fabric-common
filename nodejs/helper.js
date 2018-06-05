2e67e8617495d4fae7e77a52075cc6085826b3f6
davidliu <david-khala@hotmail.com>
2018-06-05 15:43:50 +0800

    move to common helper

--- nodejs/helper.js
+++ nodejs/helper.js
@@ -3,3 +3,4 @@ exports.randomKeyOf = (obj) => {
 	const keyIndex = Math.floor(Math.random() * Math.floor(keys.length));
 	return keys[keyIndex];
 };
+exports.JSONReadable = (data) => JSON.stringify(data, null, 2);
