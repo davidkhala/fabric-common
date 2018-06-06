299c78748ed80a60514b23a5006ab2acf5cf8891
davidliu <david-khala@hotmail.com>
2018-06-06 11:10:52 +0800

    TODO nodejs hang problem

--- bin-manage/runConfigtxlator.js
+++ bin-manage/runConfigtxlator.js
@@ -3,9 +3,10 @@ const exec = util.promisify(require('child_process').exec);
 const logger = require('../nodejs/logger').new('configtxlator server');
 const path = require('path');
 
-const run = async () => {
-	const shellScript=path.resolve(__dirname,'runConfigtxlator.sh');
-	const std = await exec(shellScript);
-	logger.info(std.stdout);
+exports.run = async (action = '') => {
+	const shellScript = path.resolve(__dirname, `runConfigtxlator.sh ${action}`);
+	logger.debug(shellScript);
+	const {stdout} = await exec(shellScript);
+	logger.info(stdout);
+	return stdout;
 };
-run();
