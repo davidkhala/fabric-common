90cc241f7c0d828dbee4019fcabef810d84e1e9d
davidliu <david-khala@hotmail.com>
2018-06-06 11:34:40 +0800

    fix pm2 attach problem

--- bin-manage/runConfigtxlator.sh
+++ bin-manage/runConfigtxlator.sh
@@ -3,7 +3,7 @@ CURRENT=$(cd $(dirname ${BASH_SOURCE}); pwd)
 
 BIN_PATH="$(dirname $CURRENT)/bin"
 
-function start(){
+function up(){
 	# NOTE: some unknown process occupied 7059 when no configtxlator => [tcp6       0      0 127.0.0.1:7059          127.0.0.1:60350         TIME_WAIT   -]
 	if netstat -pant | grep '7059' | grep 'LISTEN'; then
 		echo 7059 occupied, skip
@@ -16,7 +16,9 @@ function down(){
     pid=$(netstat -pant | grep '7059' | grep 'LISTEN' | awk '{split($7, a, "/");print a[1]}')
 	if [ -n "$pid" ]; then
 		kill $pid # kill with no arguments: kill: usage: kill [-s sigspec | -n signum | -sigspec] pid | jobspec ... or kill -l [sigspec]
-	    echo kill $pid
+	    echo kill $pid configtxlator
+	else
+        echo "no process found for configtxlator"
 	fi
 }
 ACTION=$1
@@ -24,5 +26,5 @@ if [ -n "$ACTION" ]; then
     $ACTION
 else
     down
-    start
+    up
 fi
--- nodejs/express/pm2Manager.js
+++ nodejs/express/pm2Manager.js
@@ -1,36 +1,44 @@
 const pm2 = require('pm2');
 const logger = require('../logger').new('pm2 Manager');
 exports.run = async ({name, script, env}) => {
-	const exist = await exports.get({name});
-	if (exist) {
+	let process = await exports.get({name}, true);
+	if (process) {
 		logger.warn(`process ${name} exist`);
-		return exist;
-	}
-	return await new Promise((resolve, reject) => {
-		pm2.start({name, script, env}, (err, process) => {
-			if (err) reject(err);
-			resolve(process);
+	} else {
+		process = await new Promise((resolve, reject) => {
+			pm2.start({name, script, env}, (err, process) => {
+				if (err) reject(err);
+				resolve(process);
+			});
 		});
-	});
+	}
+	pm2.disconnect();
+	return process;
 };
-exports.get = ({name}) => {
-	return new Promise((resolve, reject) => {
+exports.get = async ({name}, persist) => {
+
+	const list = await new Promise((resolve, reject) => {
 		pm2.list((err, list) => {
 			if (err) reject(err);
 			resolve(list.find(({name: pName}) => pName === name));
 		});
 	});
+	if (!persist) {
+		pm2.disconnect();
+	}
+	return list;
 };
 exports.kill = async ({name}) => {
-	const exist = await exports.get({name});
+	const exist = await exports.get({name}, true);
 	if (!exist) {
 		logger.warn(`process ${name} not exist, delete skipped`);
-		return;
-	}
-	return await new Promise((resolve, reject) => {
-		pm2.delete(name, (err) => {
-			if (err) reject(err);
-			resolve();
+	} else {
+		await new Promise((resolve, reject) => {
+			pm2.delete(name, (err) => {
+				if (err) reject(err);
+				resolve();
+			});
 		});
-	});
+	}
+	pm2.disconnect();
 };
