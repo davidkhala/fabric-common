4f1f9d2f5ac0592b6cba9094986684583056f76c
davidliu <david-khala@hotmail.com>
2018-06-06 15:20:58 +0800

    FIX pm2 bug after pm2.disconnect

--- nodejs/express/pm2Manager.js
+++ nodejs/express/pm2Manager.js
@@ -1,44 +1,69 @@
 const pm2 = require('pm2');
 const logger = require('../logger').new('pm2 Manager');
-exports.run = async ({name, script, env}) => {
-	let process = await exports.get({name}, true);
-	if (process) {
-		logger.warn(`process ${name} exist`);
-	} else {
-		process = await new Promise((resolve, reject) => {
-			pm2.start({name, script, env}, (err, process) => {
-				if (err) reject(err);
-				resolve(process);
-			});
-		});
-	}
-	pm2.disconnect();
-	return process;
-};
-exports.get = async ({name}, persist) => {
+exports.PM2 = class {
+	constructor() {
 
-	const list = await new Promise((resolve, reject) => {
-		pm2.list((err, list) => {
-			if (err) reject(err);
-			resolve(list.find(({name: pName}) => pName === name));
-		});
-	});
-	if (!persist) {
-		pm2.disconnect();
 	}
-	return list;
-};
-exports.kill = async ({name}) => {
-	const exist = await exports.get({name}, true);
-	if (!exist) {
-		logger.warn(`process ${name} not exist, delete skipped`);
-	} else {
+
+	async connect() {
 		await new Promise((resolve, reject) => {
-			pm2.delete(name, (err) => {
+			pm2.connect(err => {
 				if (err) reject(err);
 				resolve();
 			});
 		});
+		return this;
 	}
-	pm2.disconnect();
+
+	disconnect() {
+		pm2.disconnect();
+	}
+
+	async run({name, script, env}) {
+		let process = await this.get({name});
+		if (process) {
+			logger.warn(`process ${name} exist`);
+		} else {
+			process = await new Promise((resolve, reject) => {
+				pm2.start({name, script, env}, (err, process) => {
+					if (err) {
+						logger.error(err);
+						reject(err);
+					}
+					resolve(process);
+				});
+			});
+			logger.info(`process ${name} started`);
+		}
+		return process;
+	}
+
+	async delete({name}) {
+		const process = await this.get({name});
+		if (!process) {
+			logger.warn(`process ${name} not exist, delete skipped`);
+		} else {
+			await new Promise((resolve, reject) => {
+				pm2.delete(name, (err) => {
+					if (err) reject(err);
+					resolve();
+				});
+			});
+		}
+		return process;
+	}
+
+	async list() {
+		return await new Promise((resolve, reject) => {
+			pm2.list((err, list) => {
+				if (err) reject(err);
+				resolve(list);
+			});
+		});
+	}
+
+	async get({name}) {
+		const list = await this.list();
+		return list.find(({name: pName}) => pName === name);
+	}
 };
