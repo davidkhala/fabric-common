23620b3e8da17055525e0c7059435b97db62cfd1
davidliu <david-khala@hotmail.com>
2018-06-07 12:01:55 +0800

    ip getter

--- nodejs/helper.js
+++ nodejs/helper.js
@@ -1,6 +1,22 @@
+const os = require('os');
 exports.randomKeyOf = (obj) => {
 	const keys = Object.keys(obj);
 	const keyIndex = Math.floor(Math.random() * Math.floor(keys.length));
 	return keys[keyIndex];
 };
 exports.JSONReadable = (data) => JSON.stringify(data, null, 2);
+exports.ip = () => {
+	const allInterfaces = os.networkInterfaces();
+	const ips = [];
+	for (const interfaceName in allInterfaces) {
+		if (interfaceName.includes('docker')) continue;
+		const Interface = allInterfaces[interfaceName];
+		for (const each of Interface) {
+			if (each.family === 'IPv4' && !each.internal) {
+				ips.push(each.address);
+			}
+		}
+	}
+	return ips;
+};
+exports.hostname = os.hostname();
