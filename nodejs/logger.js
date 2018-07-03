70c7307e1cf5b2157974d0a0f290dc4099a81e50
davidliu <david-khala@hotmail.com>
2018-07-03 18:03:54 +0800

    migrate to use winston logger

--- nodejs/logger.js
+++ nodejs/logger.js
@@ -1,11 +1,18 @@
-const Log4js = require('log4js');
+const winston = require('winston');
 exports.new = (moduleName) => {
-	const logger = Log4js.getLogger(moduleName);
-	logger.level = 'debug';
+	const logger = new (winston.Logger)({
+		transports: [
+			new (winston.transports.Console)({
+				level: 'debug',
+				colorize: true,
+				label: moduleName
+			})
+		]
+	});
 	return logger;
 };
 exports.setGlobal = () => {
-	const hfcLogger = module.exports.new('hfc');
+	const hfcLogger = exports.new('hfc');
 	global.hfc = {
 		logger: hfcLogger
 	};
