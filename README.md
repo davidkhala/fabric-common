b21bd666c20a5119b0e9ef0d2107b58b67b3c582
davidliu <david-khala@hotmail.com>
2018-07-04 11:16:13 +0800

    update golang

--- README.md
+++ README.md
@@ -3,7 +3,4 @@
 # Installation
 
 golang
->  ``$ ./install.sh golang``
->  - overwrite install
->  - if docker is found, golang version to install would be aligned to the one of docker
->  - change $PATH in `/etc/profile`,adding `/usr/local/go/bin`, `$(go env GOPATH)/bin`
+>  ``$ ./install.sh golang1_10``
--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ for ((i = 2; i <= ${#}; i++)); do
 	remain_params="$remain_params $j"
 done
 
-function golang() {
+function golang1_9() {
 	goVersion=go1.9.2
 	if [ "$1" == "remove" ]; then
 	    echo remove golang $goVersion
@@ -45,9 +45,48 @@ function golang() {
 		echo "export PATH=\$PATH:$GOPATH/bin" | sudo tee -a $systemProfile
 		export PATH=$PATH:$GOPATH/bin
 	fi
-
 }
-function golangLatest() {
+function golang1_7() {
+	goVersion=go1.7.6
+	GOPATH=$HOME/go/
+	if [ "$1" == "remove" ]; then
+	    echo remove golang $goVersion
+		sudo sed -i '/\/usr\/local\/go\/bin/d' $systemProfile
+		sudo sed -i "/${GOPATH}/d" $systemProfile
+		sudo rm -rf /usr/local/go
+		return
+	fi
+
+    echo install golang $goVersion
+	goTar=$goVersion.linux-amd64.tar.gz
+	# write path to 'go' command
+	if ! grep "/usr/local/go/bin" $systemProfile; then
+		echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a $systemProfile
+		export PATH=$PATH:/usr/local/go/bin
+	fi
+
+	if ! go version | grep $goVersion; then
+		if go version; then
+			echo ... to overwrite exiting golang at GOROOT: $(go env GOROOT)
+			sudo rm -rf $(go env GOROOT)
+		fi
+		wget https://redirector.gvt1.com/edgedl/go/${goTar}
+		sudo tar -C /usr/local -xzf ${goTar}
+		rm -f ${goTar}
+	fi
+
+	# write path to $GOPATH/bin
+	if ! grep "$GOPATH/bin" $systemProfile; then
+		echo "export PATH=\$PATH:$GOPATH/bin" | sudo tee -a $systemProfile
+		export PATH=$PATH:$GOPATH/bin
+	fi
+	# write GOPATH
+	if ! grep "$GOPATH" $systemProfile; then
+		echo "export GOPATH=${GOPATH}" | sudo tee -a $systemProfile
+		export GOPATH=$GOPATH
+	fi
+}
+function golang1_10() {
 	if [ "$1" == "remove" ]; then
 		sudo apt-get -y remove golang-go
 		sudo add-apt-repository --remove -y ppa:longsleep/golang-backports
@@ -72,7 +111,7 @@ function golang_dep() {
 }
 
 function chaincodeDevEnv(){
-    golangLatest
+    golang1_10
     install_libtool
     golang_dep
 }
