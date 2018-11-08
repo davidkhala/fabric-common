8e29d42e92bc7055dba6e69acbe9bd1c6bf84cf9
davidliu <david-khala@hotmail.com>
2018-11-08 11:49:03 +0800

    refactor to binManager.js

---
+++ nodejs/binManager.js
@@ -0,0 +1,20 @@
+const {exec} = require('khala-nodeutils/helper');
+
+const Logger = require('khala-nodeutils/logger');
+const path = require('path');
+const binShellDir = path.resolve('../bin-manage');
+exports.configtxlator = async (action = '') => {
+	const logger = Logger.new('configtxlator server');
+	const shellScript = path.resolve(binShellDir, `runConfigtxlator.sh ${action}`);
+	logger.debug(shellScript);
+	const {stdout} = await exec(shellScript);
+	logger.info(stdout);
+	return stdout;
+};
+
+exports.genAnchorPeers = async (configtxYaml,channelName,orgName, anchorTxOutputFile) => {
+	const config_dir = path.dirname(configtxYaml);
+	const runConfigtxGenShell = path.resolve(binShellDir, 'runConfigtxgen.sh');
+	const PROFILE = 'anchorPeers';
+	await exec(`export FABRIC_CFG_PATH=${config_dir} && ${runConfigtxGenShell} genAnchorPeers ${anchorTxOutputFile} ${PROFILE} ${channelName} ${orgName}`);
+};
