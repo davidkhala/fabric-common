c06d254a9fe2deb5303058c2f17a2744caa88b7f
davidLiu <david-khala@hotmail.com>
2018-11-09 15:42:50 +0800

    remove deprecated helper.exec

--- nodejs/helper.js
+++ nodejs/helper.js
@@ -1,9 +1,1 @@
 exports.sha2_256 = require('fabric-client/lib/hash').SHA2_256;
-
-
-/**
- * @deprecated use khala-nodeutils
- *
- */
-const util = require('util');
-exports.exec = util.promisify(require('child_process').exec);
