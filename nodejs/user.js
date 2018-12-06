2dfaa8e4942f9f2844450b1a598b7666997040fc
davidLiu <david-khala@hotmail.com>
2018-12-06 16:44:52 +0800

    mspid

--- nodejs/user.js
+++ nodejs/user.js
@@ -8,21 +8,21 @@ const User = require('fabric-client/lib/User');
  *
  * @param cryptoPath
  * @param nodeType
- * @param mspId
+ * @param mspid
  * @param cryptoSuite
  * @returns {Promise<User>}
  */
-exports.loadFromLocal = async (cryptoPath, nodeType, mspId, cryptoSuite = clientUtil.newCryptoSuite()) => {
+exports.loadFromLocal = async (cryptoPath, nodeType, mspid, cryptoSuite = clientUtil.newCryptoSuite()) => {
 	const username = cryptoPath.userName;
 	const exist = cryptoPath.cryptoExistLocal(`${nodeType}User`);
 	if (!exist) return;
 	const {keystore, signcerts} = exist;
 
-	const user = await exports.build(username, {key: fs.readFileSync(keystore), certificate: fs.readFileSync(signcerts)}, mspId, cryptoSuite);
+	const user = await exports.build(username, {key: fs.readFileSync(keystore), certificate: fs.readFileSync(signcerts)}, mspid, cryptoSuite);
 
 	return user;
 };
-exports.build = async (username, {key, certificate}, MSPID, cryptoSuite = clientUtil.newCryptoSuite()) => {
+exports.build = async (username, {key, certificate}, mspid, cryptoSuite = clientUtil.newCryptoSuite()) => {
 	const user = new User(username);
 	let privateKey;
 	if (key instanceof ECDSA_KEY) {
@@ -32,7 +32,7 @@ exports.build = async (username, {key, certificate}, MSPID, cryptoSuite = client
 		privateKey = await cryptoSuite.importKey(key, {ephemeral: true});
 	}
 	user.setCryptoSuite(cryptoSuite);
-	await user.setEnrollment(privateKey, certificate, MSPID);
+	await user.setEnrollment(privateKey, certificate, mspid);
 	return user;
 };
 exports.getCertificate = (user) => user.getSigningIdentity()._certificate;
