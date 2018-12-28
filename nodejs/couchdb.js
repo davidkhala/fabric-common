2d34e25182c6e1db4119fc56ceee8c55754ca9da
davidLiu <david-khala@hotmail.com>
2018-12-28 16:55:10 +0800

    make nodejs version of chaincode.sh couchDBIndex

--- nodejs/couchdb.js
+++ nodejs/couchdb.js
@@ -25,3 +25,31 @@ exports.queryBuilder = (sorts = [], direction = 0) => {
 	};
 	return JSON.stringify(query);
 };
+
+const path = require('path');
+const fsExtra = require('khala-nodeutils/helper').fsExtra;
+/**
+ * overwrite couchdb index file
+ * @param {string} metaINFPath file path
+ * @param {string} fileName should ends with ".json" default: index.json
+ * @param {string} fields sorting fields
+ */
+exports.couchDBIndex = (metaINFPath, fileName, ...fields) => {
+	if (!fileName) {
+		fileName = 'index.json';
+	} else {
+		if (!fileName.endsWith('.json')) {
+			const err = Error(`fileName should endsWith ".json", but got ${fileName}`);
+			err.code = 'couchdb';
+			throw err;
+		}
+	}
+	const target = path.resolve(metaINFPath, 'statedb', 'couchdb', 'indexes', fileName);
+	const object = {
+		index: {
+			fields,
+			type: 'json'
+		}
+	};
+	fsExtra.outputJsonSync(target, object);
+};
