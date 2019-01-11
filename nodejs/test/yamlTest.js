c5d728400d745803365f0b1ef00cc6f70a32a3b9
davidLiu <david-khala@hotmail.com>
2019-01-11 15:12:43 +0800

    test: parse sample configtx.yaml

---
+++ nodejs/test/yamlTest.js
@@ -0,0 +1,8 @@
+const {nodeUtil} =require('../helper')
+const yaml = nodeUtil.yaml();
+const logger = nodeUtil.devLogger('yaml')
+
+const path = require('path');
+const configtxFile = path.resolve(__dirname, '../../config/configtx.yaml');
+const configtxObj = yaml.read(configtxFile);
+logger.debug(configtxObj.Profiles);
