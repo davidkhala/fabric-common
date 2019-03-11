504b69838bbb67843ae65ec303302bf184f58e81
davidLiu <david-khala@hotmail.com>
2019-03-11 15:21:38 +0800

    vsLint

--- bash/peer.sh
+++ bash/peer.sh
@@ -5,20 +5,20 @@ CORE_PEER_TLS_CERT_FILE=""
 CORE_PEER_TLS_ROOTCERT_FILE=""
 ordererEndpoint="orderer1.test.mediconcen.com:7050"
 ordererHostname=""
-function channelList(){
+function channelList() {
     local CMD="peer channel list --tls --cafile=$CORE_PEER_TLS_ROOTCERT_FILE --certfile=$CORE_PEER_TLS_CERT_FILE --keyfile=$CORE_PEER_TLS_KEY_FILE"
-    if [[ -n ${ordererHostname} ]];then
+    if [[ -n ${ordererHostname} ]]; then
         CMD="$CMD --ordererTLSHostnameOverride=$ordererHostname"
     fi
     $CMD
 
 }
-function channelConfig(){
+function channelConfig() {
     local channelName=$1
 
     local CMD="peer channel fetch --tls --cafile=$CORE_PEER_TLS_ROOTCERT_FILE --certfile=$CORE_PEER_TLS_CERT_FILE --keyfile=$CORE_PEER_TLS_KEY_FILE -c=$channelName -o=$ordererEndpoint"
 
-    if [[ -n ${ordererHostname} ]];then
+    if [[ -n ${ordererHostname} ]]; then
         CMD="$CMD --ordererTLSHostnameOverride=$ordererHostname"
     fi
     $CMD
