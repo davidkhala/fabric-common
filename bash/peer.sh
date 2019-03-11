1283055b3ba98e97bb4c0e42f27ff0fbef2866bf
davidLiu <david-khala@hotmail.com>
2019-03-11 15:21:05 +0800

    peer channel list is independent from orderer

--- bash/peer.sh
+++ bash/peer.sh
@@ -6,7 +6,7 @@ CORE_PEER_TLS_ROOTCERT_FILE=""
 ordererEndpoint="orderer1.test.mediconcen.com:7050"
 ordererHostname=""
 function channelList(){
-    local CMD="peer channel list --tls --cafile=$CORE_PEER_TLS_ROOTCERT_FILE --certfile=$CORE_PEER_TLS_CERT_FILE --keyfile=$CORE_PEER_TLS_KEY_FILE -o=$ordererEndpoint"
+    local CMD="peer channel list --tls --cafile=$CORE_PEER_TLS_ROOTCERT_FILE --certfile=$CORE_PEER_TLS_CERT_FILE --keyfile=$CORE_PEER_TLS_KEY_FILE"
     if [[ -n ${ordererHostname} ]];then
         CMD="$CMD --ordererTLSHostnameOverride=$ordererHostname"
     fi
