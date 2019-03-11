2121b2b7a63ba8c9b73d42063f1f69c066922898
davidLiu <david-khala@hotmail.com>
2019-03-11 15:15:19 +0800

    add bash helper

---
+++ bash/peer.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CORE_PEER_TLS_KEY_FILE=""
+CORE_PEER_TLS_CERT_FILE=""
+CORE_PEER_TLS_ROOTCERT_FILE=""
+ordererEndpoint="orderer1.test.mediconcen.com:7050"
+ordererHostname=""
+function channelList(){
+    local CMD="peer channel list --tls --cafile=$CORE_PEER_TLS_ROOTCERT_FILE --certfile=$CORE_PEER_TLS_CERT_FILE --keyfile=$CORE_PEER_TLS_KEY_FILE -o=$ordererEndpoint"
+    if [[ -n ${ordererHostname} ]];then
+        CMD="$CMD --ordererTLSHostnameOverride=$ordererHostname"
+    fi
+
+
+}
+function channelConfig(){
+    local channelName=$1
+
+    local CMD="peer channel fetch --tls --cafile=$CORE_PEER_TLS_ROOTCERT_FILE --certfile=$CORE_PEER_TLS_CERT_FILE --keyfile=$CORE_PEER_TLS_KEY_FILE -c=$channelName -o=$ordererEndpoint"
+
+    if [[ -n ${ordererHostname} ]];then
+        CMD="$CMD --ordererTLSHostnameOverride=$ordererHostname"
+    fi
+}
