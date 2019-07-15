d3e4c39c40eb2788570dbeb5fb75bac7209dd085
davidkhala <david-khala@hotmail.com>
2019-07-15 09:58:13 +0800

    0.0.5: add index file

---
+++ nodejs/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	Channel: require('./channel'),
+	EventHub: require('./eventHub'),
+	Chaincode: require('./chaincode'),
+	ChaincodeHelper: require('./chaincodeHelper'),
+	Client: require('./client')
+};
