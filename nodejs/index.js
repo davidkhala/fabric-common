c4cde1c34de931c12823fb21069db5a2ab0945a1
davidkhala <david-khala@hotmail.com>
2019-07-16 01:46:38 +0800

    0.0.6: add more property to index

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,19 @@
 module.exports = {
 	Channel: require('./channel'),
+	Peer: require('./peer'),
+	Path: require('./path'),
+	User: require('./user'),
+	Orderer: require('./orderer'),
 	EventHub: require('./eventHub'),
+	CA: require('./ca'),
+	CACryptoGen: require('./ca-crypto-gen'),
 	Chaincode: require('./chaincode'),
 	ChaincodeHelper: require('./chaincodeHelper'),
-	Client: require('./client')
+	Client: require('./client'),
+	Helper: require('./helper'),
+	Golang: require('./golang'),
+	Policy: require('./policy'),
+	PrivateData: require('./privateData'),
+	BinaryManager: require('./binManager'),
+	Configtxlator: require('./configtxlator')
 };
