f5967a1481f34e6fef8efa968c03488a12f0262c
davidkhala <david-khala@hotmail.com>
2019-07-20 21:26:33 +0800

    clean runConfigtslator

--- bin-manage/runConfigtxlator.sh
+++ bin-manage/runConfigtxlator.sh
@@ -3,8 +3,7 @@ CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
 
 BIN_PATH="$(dirname $CURRENT)/bin"
 
-function up() {
-	# NOTE: some unknown process occupied 7059 when no configtxlator => [tcp6       0      0 127.0.0.1:7059          127.0.0.1:60350         TIME_WAIT   -]
+up() {
 	if netstat -pant | grep '7059' | grep 'LISTEN'; then
 		echo 7059 occupied, skip
 	else
@@ -12,18 +11,18 @@ function up() {
 		echo configtxlator start
 	fi
 }
-function down() {
+down() {
 	pid=$(netstat -pant | grep '7059' | grep 'LISTEN' | awk '{split($7, a, "/");print a[1]}')
-	if [ -n "$pid" ]; then
+	if [[ -n "$pid" ]]; then
 		kill $pid # kill with no arguments: kill: usage: kill [-s sigspec | -n signum | -sigspec] pid | jobspec ... or kill -l [sigspec]
 		echo kill $pid configtxlator
 	else
 		echo "no process found for configtxlator"
 	fi
 }
-ACTION=$1
-if [ -n "$ACTION" ]; then
-	$ACTION
+$1
+if [[ -n "$1" ]]; then
+	$1
 else
 	down
 	up
