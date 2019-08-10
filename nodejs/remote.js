29e02396d052c3a59dce0afe32fccec9b32e75e9
david <david-khala@hotmail.com>
2019-08-10 12:29:20 +0800

    when ssl-target-name-override, clientKey and clientCert is not used

--- nodejs/remote.js
+++ nodejs/remote.js
@@ -1,8 +1,12 @@
+const logger = require('./logger').new('remote');
 const RemoteOptsTransform = (opts = {}) => {
 	const {sslTargetNameOverride} = opts;
 	if (sslTargetNameOverride) {
 		opts['ssl-target-name-override'] = sslTargetNameOverride;
+		logger.warn(`[ssl-target-name-override]=${sslTargetNameOverride} used for test environment only when the server certificate's hostname ('CN') does not match the actual host endpoint`);
 		delete opts.sslTargetNameOverride;
+		delete opts.clientKey;
+		delete opts.clientCert;
 	}
 	for (const [key, value] of Object.entries(opts)) {
 		if (!value) {
