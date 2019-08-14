d5476739c045921c9564add563acd8dd06c3d0f0
david <david-khala@hotmail.com>
2019-08-14 14:58:42 +0800

    binManagerTest: add coverage

--- nodejs/test/binManageTest.js
+++ nodejs/test/binManageTest.js
@@ -2,30 +2,62 @@ const path = require('path');
 process.env.binPath = path.resolve(__dirname, '../../bin');
 const BinManager = require('../binManager');
 const binManager = new BinManager();
+const configtxYaml = path.resolve(__dirname, '../../config/configtx.yaml');
+const logger = require('../logger').new('test:binManager', true);
+const blockProfiles = [
+	'SampleSingleMSPSolo',
+	'SampleSingleMSPKafka',
+	'SampleInsecureSolo',
+	'SampleInsecureKafka',
+	'SampleDevModeSolo',
+	'SampleDevModeKafka',
+	'SampleDevModeEtcdRaft'
+];
+const channelProfiles = ['SampleSingleMSPChannel'];
 
+const genBlockTest = async (blockProfile) => {
+	const blockFile = path.resolve(__dirname, `artifacts/${blockProfile}.block`);
+	await binManager.configtxgen(blockProfile, configtxYaml).genBlock(blockFile);
+	logger.info(`genBlock[${blockProfile}]`);
+};
 
-const viewBlockTask = async () => {
-	const blockProfile = 'delphiGenesis';
-	const blockFile = '/home/davidliu/Documents/delphi-fabric/config/configtx/delphi.block';
-	const configtxYaml = '/home/davidliu/Documents/delphi-fabric/config/configtx.yaml';
-	const resultJSObject = await binManager.configtxgen(blockProfile, configtxYaml).viewBlock(blockFile);
-	console.info(resultJSObject);
+const viewBlockTest = async (blockProfile) => {
+	const blockFile = path.resolve(__dirname, `artifacts/${blockProfile}.block`);
+	const result = await binManager.configtxgen(blockProfile, configtxYaml).viewBlock(blockFile);
+	logger.info(`viewBlock[${blockProfile}]`, result);
 };
-const viewChannelTxTask = async () => {
-	const channelName = 'allchannel';
-	const channelProfile = 'allchannel';
-	const configtxYaml = '/home/davidliu/Documents/delphi-fabric/config/configtx.yaml';
-	const channeltxFile = '/home/davidliu/Documents/delphi-fabric/config/configtx/all.tx';
-	const resultJSObject = await binManager.configtxgen(channelProfile, configtxYaml, channelName).viewChannel(channeltxFile);
-	console.info(resultJSObject);
+const genChannelTest = async (channelProfile, channelName = channelProfile) => {
+	const channelFile = path.resolve(__dirname, `artifacts/${channelProfile}.tx`);
+	await binManager.configtxgen(channelProfile, configtxYaml, channelName).genChannel(channelFile);
+	logger.info(`genChannel[${channelProfile}]`);
 };
+const viewChannelTest = async (channelProfile, channelName = channelProfile) => {
+	const channelFile = path.resolve(__dirname, `artifacts/${channelProfile}.tx`);
+	const result = await binManager.configtxgen(channelProfile, configtxYaml, channelName).viewChannel(channelFile);
+	logger.info(`viewChannel[${channelProfile}]`, result);
+};
+
 const configtxlatorRestart = async () => {
-	await binManager.configtxlator('down');
 	await binManager.configtxlator('start');
+	await binManager.configtxlator('down');
 };
-const task = async () => {
+const taskConfigtxlator = async () => {
 	await configtxlatorRestart();
-
 };
 
-task();
+const taskConfigtxgen = async () => {
+	for (const profile of blockProfiles) {
+		if (profile === 'SampleDevModeEtcdRaft') {
+			logger.warn('skip SampleDevModeEtcdRaft due to : path/to/ClientTLSCert0: no such file or directory');
+			continue;
+		}
+		await genBlockTest(profile);
+		await viewBlockTest(profile);
+	}
+	for (const profile of channelProfiles) {
+		await genChannelTest(profile);
+		await viewChannelTest(profile);
+	}
+};
+taskConfigtxlator();
+taskConfigtxgen();
