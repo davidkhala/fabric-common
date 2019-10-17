f076afbd89b48294d6b0d53840988e16f2a1ab17
davidLiu <david-khala@hotmail.com>
2019-10-17 18:03:22 +0800

    add log

--- nodejs/channelConfig.js
+++ nodejs/channelConfig.js
@@ -462,6 +462,7 @@ exports.channelUpdate = async (channel, orderer, configChangeCallback, signature
 
 	const updateChannelResp = await client.updateChannel(request);
 	if (updateChannelResp.status !== 'SUCCESS') {
+		logger.error(updateChannelResp);
 		throw Object.assign(Error('Channel update'), updateChannelResp);
 	}
 	logger.info(`[${channelName}] channel update: ${updateChannelResp}`);
