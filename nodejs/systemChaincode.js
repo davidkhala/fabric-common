06967d967273fd2fb5a8288b4aa573053a1cf2b9
david <david-khala@hotmail.com>
2019-10-18 16:00:15 +0800

    backup

--- nodejs/systemChaincode.js
+++ nodejs/systemChaincode.js
@@ -15,3 +15,14 @@ exports.systemChaincodes = systemChaincodes;
 exports.getActionSet = (systemChaincode) => {
 	return Object.keys(systemChaincodes[systemChaincode]).concat(Object.values(systemChaincodes[systemChaincode])).filter(e => e);
 };
+
+
+exports.ChaincodeExists = (channelName, chaincodeId) => {
+	return {chaincodeId: 'lscc', fcn: 'ChaincodeExists', args: [channelName, chaincodeId]};
+};
+exports.GetChaincodeData = (channelName, chaincodeId) => {
+	return {chaincodeId: 'lscc', fcn: 'GetChaincodeData', args: [channelName, chaincodeId]};
+};
+exports.GetDeploymentSpec = (channelName, chaincodeId) => {
+	return {chaincodeId: 'lscc', fcn: 'GetDeploymentSpec', args: [channelName, chaincodeId]};
+};
