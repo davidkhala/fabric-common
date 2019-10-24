604d9238c4c746c55efe2470d30f23ddabc2279d
davidLiu <david-khala@hotmail.com>
2019-10-24 17:33:25 +0800

    [WIP]backup

--- nodejs/peer.js
+++ nodejs/peer.js
@@ -147,6 +147,15 @@ exports.envBuilder = ({network, msp: {configPath, id, peerHostName}, tls, couchD
 		environment = environment.concat([
 			`CORE_METRICS_PROVIDER=${MetricsProvider[provider]}`
 		]);
+		if (provider === MetricsProvider.StatsD) {
+			const {statsD: {host}} = metricsOpts;
+			environment = environment.concat([
+				'CORE_METRICS_STATSD_NETWORK=udp',
+				`CORE_METRICS_STATSD_ADDRESS=${host}:8125`,
+				`CORE_METRICS_STATSD_PREFIX=${peerHostName}`,
+				'CORE_METRICS_STATSD_WRITEINTERVAL=9s' // TODO 9 seconds do not work??
+			]);
+		}
 	}
 	return environment;
 };
