923bb657cc39f28603ad4660cf4f74833d66fb3d
david <david-khala@hotmail.com>
2019-11-19 14:15:21 +0800

    classify operation.js

--- nodejs/operations.js
+++ nodejs/operations.js
@@ -11,51 +11,71 @@ const {LoggingLevel} = require('./remote');
  * @property {string} CommitSHA '7917a40'
  * @property {string} Version
  */
+
 /**
  * @typedef {Object} OperationLogSpec
  * @property {string} spec
  */
 
-/**
- * /healthz official health check, for peer and orderer
- * @param {string} baseUrl ${domain}:${port} port is usually 9443(for peer), 8443(for orderer)
- * @param {RequestExtraOptions} [otherOptions]
- * @returns {Promise<OperationHealthz>}
- */
-exports.health = async (baseUrl, otherOptions) => {
-	const url = `${baseUrl}/healthz`;
-	const result = await RequestPromise({url, method: 'GET'}, otherOptions);
-	if (result.status === 'OK') {
-		return result;
-	} else {
-		const err = Error('healthz check');
-		err.url = url;
-		Object.assign(err, result);
-		throw err;
-	}
-};
-
-/**
- *
- * @param {string} baseUrl
- * @param {RequestExtraOptions} [otherOptions]
- * @return {Promise<string>} spec
- */
-exports.getLogLevel = async (baseUrl, otherOptions) => {
-	const url = `${baseUrl}/logspec`;
+class OperationService {
 	/**
-	 * @type OperationLogSpec
+	 *
+	 * @param baseUrl ${domain}:${port} port is usually 9443(for peer), 8443(for orderer)
+	 * @param {RequestExtraOptions} [otherOptions]
 	 */
-	const result = await RequestPromise({url, method: 'GET'}, otherOptions);
-	return result.spec;
-};
-/**
- * log level validation will be completed by service
- * @param {string} baseUrl
- * @param {LoggingLevel} level
- * @param {RequestExtraOptions} [otherOptions]
- */
-exports.setLogLevel = async (baseUrl, level, otherOptions) => {
-	const url = `${baseUrl}/logspec`;
-	await RequestPromise({url, method: 'PUT', body: {spec: LoggingLevel[level]}}, otherOptions); // body is undefined
-};
+	constructor(baseUrl, otherOptions) {
+		this.url = baseUrl;
+		this.options = otherOptions;
+	}
+
+	/**
+	 * /healthz health check, for peer and orderer
+	 * @returns {Promise<OperationHealthz>}
+	 */
+	async health() {
+		const url = `${this.url}/healthz`;
+		const result = await RequestPromise({url, method: 'GET'}, this.options);
+		if (result.status === 'OK') {
+			return result;
+		} else {
+			const err = Error('healthz check');
+			err.url = url;
+			Object.assign(err, result);
+			throw err;
+		}
+	}
+
+	/**
+	 *
+	 * @return {Promise<OperationVersion>}
+	 */
+	async version() {
+		const url = `${this.url}/version`;
+		return await RequestPromise({url, method: 'GET'}, this.options);
+	}
+
+	/**
+	 *
+	 * @return {Promise<string>} spec
+	 */
+	async getLogLevel() {
+		const url = `${this.url}/logspec`;
+		/**
+		 * @type OperationLogSpec
+		 */
+		const result = await RequestPromise({url, method: 'GET'}, this.options);
+		return result.spec;
+	}
+
+	/**
+	 * log level validation will be done by service
+	 * @param {LoggingLevel} level
+	 */
+	async setLogLevel(level) {
+		const url = `${this.url}/logspec`;
+		await RequestPromise({url, method: 'PUT', body: {spec: LoggingLevel[level]}}, this.options); // body is undefined
+	}
+}
+
+module.exports = OperationService;
+
--- nodejs/test/operationTest.js
+++ nodejs/test/operationTest.js
@@ -1,13 +1,14 @@
 const peerUrl = 'https://localhost:9443';
 const ordererUrl = 'https://localhost:8443';
-const operationUtil = require('../operations');
+const OperationService = require('../operations');
 const logger = require('../logger').new('test:operation', true);
 const task = async () => {
 	let isHealth;
 	const httpsOptions = {rejectUnauthorized: false, json: true};
-	isHealth = await operationUtil.health(peerUrl, httpsOptions);
+	const service = new OperationService(peerUrl, httpsOptions);
+	isHealth = await service.health();
 	logger.info(peerUrl, isHealth);
-	isHealth = await operationUtil.health(ordererUrl, httpsOptions);
+	isHealth = await service.health();
 	logger.info(ordererUrl, isHealth);
 
 };
