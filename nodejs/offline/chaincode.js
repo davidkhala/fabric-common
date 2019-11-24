9384884037435c30bb35f71de11450a7324d789c
david <david-khala@hotmail.com>
2019-11-24 17:57:38 +0800

    work around for sdk bug

--- nodejs/offline/chaincode.js
+++ nodejs/offline/chaincode.js
@@ -74,11 +74,12 @@ exports.unsignedTransactionProposal = (channelName, {fcn, args = [], chaincodeId
 exports.unsignedTransaction = (channelName, proposalResponses, proposal) => {
 	const channel = emptyChannel(channelName);
 
+	const proposalCopy = Object.assign({getHeader: () => proposal.header}, proposal); // TODO work around of sdk bug
 	/**
 	 * @type {TransactionRequest}
 	 */
 	const request = {
-		proposalResponses, proposal,
+		proposalResponses, proposal: proposalCopy,
 		orderer: undefined, txID: undefined
 	};
 	return channel.generateUnsignedTransaction(request);
