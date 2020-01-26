07f1fd304d2e5c23ed9b381c53907be542dc8272
davidliu <david-khala@hotmail.com>
2020-01-26 12:43:35 +0800

    0.0.1: add getPayloads

--- nodejs/formatter/txProposal.js
+++ nodejs/formatter/txProposal.js
@@ -1,13 +1,19 @@
-exports.proposalStringify = (proposalResponse) => {
+const proposalStringify = (proposalResponse) => {
 	if (!(proposalResponse instanceof Error)) {
 		proposalResponse.response.payload = proposalResponse.response.payload.toString();
 	}
 	return proposalResponse;
 };
-exports.proposalFlatten = proposalResponse => {
+const proposalFlatten = proposalResponse => {
 	if (proposalResponse instanceof Error) {
 		return proposalResponse.message;
 	} else {
 		return proposalResponse.response.payload;
 	}
 };
+
+exports.getPayloads = ({proposalResponses}) => {
+	return proposalResponses.map((entry) => proposalFlatten(proposalStringify(entry)));
+};
+exports.proposalStringify = proposalStringify;
+exports.proposalFlatten = proposalFlatten;
