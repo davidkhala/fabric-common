fb8bbcbb48b8beaa3012c691a400caf9560e8e98
david <david-khala@hotmail.com>
2020-04-24 14:59:42 +0800

    prune proposalStringify

--- nodejs/formatter/txProposal.js
+++ nodejs/formatter/txProposal.js
@@ -1,31 +1,19 @@
 /**
- * TODO payload  protobuf deserialize
- * @param {Client.ProposalResponse} proposalResponse
- * @return {Client.ProposalResponse}
- */
-const proposalStringify = (proposalResponse) => {
-	if (!(proposalResponse instanceof Error)) {
-		proposalResponse.response.payload = proposalResponse.response.payload.toString();
-	}
-	return proposalResponse;
-};
-/**
  *
- * @param proposalResponse
- * @return {string|Buffer}
+ * @param {Client.ProposalResponse} proposalResponse
+ * @return {string}
  */
 const proposalFlatten = proposalResponse => {
 	if (proposalResponse instanceof Error) {
 		return proposalResponse.message;
 	} else {
-		return proposalResponse.response.payload;
+		return proposalResponse.response.payload.toString('utf8');
 	}
 };
 
 exports.getPayloads = ({proposalResponses}) => {
-	return proposalResponses.map((entry) => proposalFlatten(proposalStringify(entry)));
+	return proposalResponses.map((entry) => proposalFlatten(entry));
 };
-exports.proposalStringify = proposalStringify;
 exports.proposalFlatten = proposalFlatten;
 
 /**
