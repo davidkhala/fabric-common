2312678993a29031638845fc0ea75aa198b54f09
david <david-khala@hotmail.com>
2020-05-17 13:57:51 +0800

    [WIP] setupAnchorPeer green enumerate types

--- nodejs/formatter/configtxlator.js
+++ nodejs/formatter/configtxlator.js
@@ -4,7 +4,10 @@
  */
 const ConfigtxlatorType = {
 	Config: 'common.Config',
-	ConfigUpdate: 'common.ConfigUpdate'
+	ConfigUpdate: 'common.ConfigUpdate',
+	Envelope: 'common.Envelope',
+	Policy: 'common.Policy',
+	Block: 'common.Block',
 };
 module.exports = {
 	ConfigtxlatorType,
