ccf73e17ec1a750eff17ea045eab2e9e942b4b4d
david <david-khala@hotmail.com>
2020-06-22 14:02:59 +0800

    missing path.js in published version

--- nodejs/test/hsmTest.js
+++ nodejs/test/hsmTest.js
@@ -2,24 +2,18 @@ const HSM = require('../hsm');
 const logger = require('khala-logger/log4js').consoleLogger('test:hsm');
 logger.info('libs', HSM.availablePKCSLibs);
 
-/**
- * Test generate ephemeral ECDSA key pair, sign, and verify
- * @return {Promise<void>}
- * @constructor
- */
-const ECDSATask = async (cryptoSuite, ephemeral) => {
 
-	const message = Buffer.from('hello');
-	const key = await cryptoSuite.generateKey({algorithm: 'ECDSA', ephemeral});
-	const sig = cryptoSuite.sign(key, message, null);
-	const verifyResult = cryptoSuite.verify(key, sig, message);
-	logger.info('ECDSATask:verifyResult', verifyResult);
-};
 describe('HSM', () => {
 	const slot = 0;
 	const pin = 'fabric';
-	it('ECDSA', async () => {
+
+	it('generate ephemeral ECDSA key pair, sign, and verify', async () => {
 		const cryptoSuite = HSM.newHSMCryptoSuite({slot, pin});
-		await ECDSATask(cryptoSuite, true);
+		const message = Buffer.from('hello');
+		const key = cryptoSuite.generateEphemeralKey({algorithm: 'ECDSA'});
+		const sig = cryptoSuite.sign(key, message, null);
+		// TODO (node:6461) [DEP0079] DeprecationWarning: Custom inspection function on Objects via .inspect() is deprecated
+		const verifyResult = cryptoSuite.verify(key, sig, message);
+		logger.info('ECDSATask:verifyResult', verifyResult);
 	});
 });
