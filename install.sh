7ee5377b6a7de4c82d2e4f15a4b942826828c58d
davidliu <david-khala@hotmail.com>
2020-07-13 16:43:14 +0800

    [WIP] Debug travis [WIP] brew install softhsm
    Signed-off-by: davidliu <david-khala@hotmail.com>

--- install.sh
+++ install.sh
@@ -18,29 +18,10 @@ java() {
 	sudo apt install -y default-jdk
 }
 softHSMInstall() {
-	if [[ $(uname) == "Darwin" ]]; then
-		brew install softhsm
-		#        A CA file has been bootstrapped using certificates from the SystemRoots
-		#keychain. To add additional certificates (e.g. the certificates added in
-		#the System keychain), place .pem files in
-		#  /usr/local/etc/openssl/certs
-		#
-		#and run
-		#  /usr/local/opt/openssl/bin/c_rehash
-		#
-		#openssl is keg-only, which means it was not symlinked into /usr/local,
-		#because Apple has deprecated use of OpenSSL in favor of its own TLS and crypto libraries.
-		#
-		#If you need to have openssl first in your PATH run:
-		#  echo 'export PATH="/usr/local/opt/openssl/bin:$PATH"' >> ~/.bash_profile
-		#
-		#For compilers to find openssl you may need to set:
-		#  export LDFLAGS="-L/usr/local/opt/openssl/lib"
-		#  export CPPFLAGS="-I/usr/local/opt/openssl/include"
-	else
+#	 FIXME: for macOS
 	  sudo apt update
 		sudo apt-get install -y softhsm2
-	fi
+
 }
 fabricInstall() {
 	#	If you want the latest production release, omit all version identifiers.
