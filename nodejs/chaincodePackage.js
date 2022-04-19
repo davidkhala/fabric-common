f53fe819b131a2f9da51dc693c9c0407d00278cd
DavidLiu <david.yx.liu@oracle.com>
2022-04-19 22:36:36 +0800

    before implement external builder
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

--- nodejs/chaincodePackage.js
+++ nodejs/chaincodePackage.js
@@ -45,25 +45,18 @@ export default class ChaincodePackage {
 			fs.writeFileSync(path.resolve(tmpRoot, 'metadata.json'), JSON.stringify({path: modulePath, type: Type, label: Label}));
 
 			const couchdbDir = path.resolve(Path, 'META-INF', 'statedb', 'couchdb');
+			const [tmpRoot2, t2] = createTmpDir();
+			fsExtra.copySync(Path, path.resolve(tmpRoot2, 'src'));
 			if (isDirectory(couchdbDir)) {
-				const [tmpRoot2, t2] = createTmpDir();
-
 
 				fsExtra.moveSync(path.resolve(Path, 'META-INF'), path.resolve(tmpRoot2, 'META-INF'));// NOTE: The move operation eliminate the META-INF autogen footprint
 				fsExtra.copySync(Path, path.resolve(tmpRoot2, 'src'));
-				await compress.tgz.compressDir(tmpRoot2, path.resolve(tmpRoot, 'code.tar.gz'), {
-					ignoreBase: true,
-				});
 
-				t2();
-			} else {
-				await compress.tgz.compressDir(Path, path.resolve(tmpRoot, 'code.tar.gz'), {
-					ignoreBase: true,
-					relativePath: 'src'
-				});
 			}
-
-
+			await compress.tgz.compressDir(tmpRoot2, path.resolve(tmpRoot, 'code.tar.gz'), {
+				ignoreBase: true,
+			});
+			t2()
 			await compress.tgz.compressDir(tmpRoot, output, {
 				ignoreBase: true
 			});
