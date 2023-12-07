102ba07e46c6816f92b6b2c0ea842dbead6fba2b
David <david-khala@hotmail.com>
2023-12-07 14:16:21 +0800

    fixing CI

--- bash/orderer.sh
+++ bash/orderer.sh
@@ -1,12 +1,32 @@
 set -e
 
 
-
-ordererJoin() {
-  # TODO WIP where is osnadmin
-    local PRIVATE_KEY=$1
-    local SIGN_CERT=$2
-    local CA=$3
-    local CHANNEL_NAME=$CHANNEL_NAME
-    osnadmin channel join --channelID $CHANNEL_NAME --config-block $working_dir/channel-artifacts/${CHANNEL_NAME}.block -o orderer0.${ORG_DOMAIN}:7053 --ca-file "$CA" --client-cert "$SIGN_CERT" --client-key "$PRIVATE_KEY"
+docker_exec(){
+  docker exec fabric-cli osnadmin "$@"
 }
+docker_cp(){
+  path_in_container="/opt/$(basename $1)"
+    docker cp -q $1 fabric-cli:$path_in_container
+    echo $path_in_container
+
+}
+
+join_channel() {
+  # TODO WIP where is osnadmin
+    local CHANNELID=$1
+    local ORDERER_ADDRESS=$2
+    local block=$3
+
+    block_in_container=$(docker_cp $block)
+    CA_FILE_in_container=$(docker_cp $CA_FILE)
+    CERT_in_container=$(docker_cp $CLIENT_CERT)
+    KEY_in_container=$(docker_cp $CLIENT_KEY)
+    docker_exec channel join --channelID=$CHANNELID -o $ORDERER_ADDRESS --config-block=$block_in_container --ca-file $CA_FILE_in_container --client-cert $CERT_in_container --client-key $KEY_in_container
+}
+list(){
+  osnadmin channel list
+}
+remove () {
+  :
+}
+"$@"
---
+++ bash/test/orderer.sh
@@ -0,0 +1,7 @@
+join_channel(){
+  export CLIENT_KEY=~/delphi-fabric/config/ca-crypto-config/ordererOrganizations/hyperledger/client/clientKey
+  export CLIENT_CERT=~/delphi-fabric/config/ca-crypto-config/ordererOrganizations/hyperledger/client/clientCert
+  export CA_FILE=~/delphi-fabric/config/ca-crypto-config/ordererOrganizations/hyperledger/msp/tlscacerts/tlsca.hyperledger-cert.pem
+  ../orderer.sh join_channel allchannel orderer0.hyperledger:9443 /home/davidliu/delphi-fabric/config/configtx/all.block
+}
+$1
