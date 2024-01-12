dd439a58b43ca6e0bd7c9de71ff861e220dbd35b
david <david-khala@hotmail.com>
2024-01-12 13:56:39 +0800

    bump version and fix tx.go

--- golang/go.mod
+++ golang/go.mod
@@ -5,19 +5,19 @@ go 1.20
 require (
 	github.com/davidkhala/goutils v1.5.9
 	github.com/davidkhala/goutils/grpc v0.0.0
-	github.com/davidkhala/protoutil v0.0.1
+	github.com/davidkhala/protoutil v0.0.2
 	github.com/hyperledger/fabric-protos-go-apiv2 v0.3.2
 	github.com/kortschak/utter v1.5.0
 	github.com/pkg/errors v0.9.1
-	google.golang.org/grpc v1.59.0
-	google.golang.org/protobuf v1.31.0
+	github.com/stretchr/testify v1.8.4
+	google.golang.org/grpc v1.60.1
+	google.golang.org/protobuf v1.32.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- golang/proto/tx.go
+++ golang/proto/tx.go
@@ -1,21 +1,62 @@
 package proto
 
 import (
+	"github.com/davidkhala/goutils"
 	"github.com/davidkhala/protoutil"
 	"github.com/hyperledger/fabric-protos-go-apiv2/common"
+	"github.com/hyperledger/fabric-protos-go-apiv2/msp"
+	"github.com/hyperledger/fabric-protos-go-apiv2/peer"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/proto"
 )
 
 type Transaction struct {
 	TxType common.HeaderType
+	*common.SignatureHeader
+	*msp.SerializedIdentity
+	*common.Config               // if TxType==common.HeaderType_CONFIG
+	*common.ConfigUpdateEnvelope // if TxType==common.HeaderType_CONFIG_UPDATE
+	*peer.ChaincodeActionPayload // if TxType==common.HeaderType_ENDORSER_TRANSACTION
+	*peer.ChaincodeAction        // if TxType==common.HeaderType_ENDORSER_TRANSACTION
+}
+
+func (t Transaction) GetType() string {
+	return common.HeaderType_name[int32(t.TxType)]
 }
 
 func ParseTransaction(txBody *common.Payload) (t Transaction) {
 	var channelHeader = GetChannelHeaderFromPayload(txBody)
 	t.TxType = common.HeaderType(channelHeader.Type)
-	protoutil.UnmarshalConfigEnvelope()
-	txBody.Data
+	t.SignatureHeader = protoutil.UnmarshalSignatureHeaderOrPanic(txBody.Header.SignatureHeader)
+	t.SerializedIdentity = GetCreatorFromSignatureHeader(t.SignatureHeader)
+	switch t.TxType {
 
+	case common.HeaderType_CONFIG:
+		config, err := protoutil.UnmarshalConfigEnvelope(txBody.Data)
+		goutils.PanicError(err)
+		t.Config = config.Config
+	case common.HeaderType_CONFIG_UPDATE:
+		configUpdateEnv := &common.ConfigUpdateEnvelope{}
+		err := proto.Unmarshal(txBody.Data, configUpdateEnv)
+		goutils.PanicError(err)
+		t.ConfigUpdateEnvelope = configUpdateEnv
+	case common.HeaderType_ENDORSER_TRANSACTION:
+		txAction := protoutil.UnmarshalTransactionActionOrPanic(txBody.Data)
+		p, a, err := protoutil.GetPayloads(txAction)
+		goutils.PanicError(err)
+		t.ChaincodeActionPayload = p
+		t.ChaincodeAction = a
+	default:
+		panic(errors.Errorf("invalid header type %s", common.HeaderType_name[channelHeader.Type]))
+	}
+
+	return
 }
 func GetChannelHeaderFromPayload(payload *common.Payload) *common.ChannelHeader {
 	return protoutil.UnmarshalChannelHeaderOrPanic(payload.Header.ChannelHeader)
+}
+func GetCreatorFromSignatureHeader(signatureHeader *common.SignatureHeader) (creator *msp.SerializedIdentity) {
+	creator, err := protoutil.UnmarshalSerializedIdentity(signatureHeader.Creator)
+	goutils.PanicError(err)
+	return creator
 }
